8d73a6540a4186f77e9a81edb2a05930f2425033
luke <test@gmail.com>
2026-02-05 20:09:56 +0000

    update

--- setup_env.sh
+++ setup_env.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-# Create a new conda environment
-conda create -y -n latentsync python=3.10.13
-conda activate latentsync
-
 # Install ffmpeg
-conda install -y -c conda-forge ffmpeg
+sudo apt -y install ffmpeg
 
 # Python dependencies
 pip install -r requirements.txt
@@ -14,5 +10,5 @@ pip install -r requirements.txt
 sudo apt -y install libgl1
 
 # Download the checkpoints required for inference from HuggingFace
-huggingface-cli download ByteDance/LatentSync-1.6 whisper/tiny.pt --local-dir checkpoints
-huggingface-cli download ByteDance/LatentSync-1.6 latentsync_unet.pt --local-dir checkpoints
+hf download ByteDance/LatentSync-1.6 whisper/tiny.pt --local-dir checkpoints
+hf download ByteDance/LatentSync-1.6 latentsync_unet.pt --local-dir checkpoints
